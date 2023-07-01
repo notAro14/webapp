256aef01d008bab9a17db638089fea47b21af0e4
Aro Andriamaro <47597252+notAro14@users.noreply.github.com>
2023-07-01 17:23:40 +0200

    Setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <main className="prose-xl prose-slate prose p-4">
+      <h1>Hello World</h1>
+      <p>React TS template</p>
+      <button className="btn-primary btn">Click Me</button>
+    </main>
+  );
+}
