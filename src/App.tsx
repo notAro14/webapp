25ddeab04f743ef9db2ccc2218d6d1c5ad129a4d
Aro Andriamaro <aro.andriamaro@aworldforus.com>
2023-10-04 13:18:04 +0200

    add css reset

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import { text } from "./components/ui/Text";
 
 export default function App() {
   return (
-    <p className={text({ size: "2xl" })}>
-      The quick brown for jumps over the lazy dog
-    </p>
+    <main style={{ background: "lightgrey", minHeight: "100dvh" }}>
+      <p className={text({ size: "2xl" })}>
+        The quick brown for jumps over the lazy dog
+      </p>
+    </main>
   );
 }
